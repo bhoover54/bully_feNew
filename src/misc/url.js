8ae3067f1461371999773b1714f7196763c63048
Afowowe Olumide Victor <volumide42@gmail.com>
2023-01-06 16:41:43 +0100

    change production server

--- src/misc/url.js
+++ src/misc/url.js
@@ -1,4 +1,6 @@
 const BASE_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:5001/" : "http://localhost:5001/"
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5001/"
+    : "https://bullyboxx-be.onrender.com/"
 
 export default BASE_URL
