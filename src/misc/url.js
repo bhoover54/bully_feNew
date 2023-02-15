aebe747b545561311ee1b96da26af5760370dcef
Afowowe Olumide Victor <volumide42@gmail.com>
2023-02-15 21:36:57 +0100

    url issue solved

--- src/misc/url.js
+++ src/misc/url.js
@@ -1,3 +1,3 @@
-const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5001/" : "https://bullyboxx-be.onrender.com/"
+const BASE_URL = process.env.NODE_ENV === "development" ? "https://bullyboxx-be.onrender.com/" : "https://bullyboxx-be.onrender.com/"
 
 export default BASE_URL
