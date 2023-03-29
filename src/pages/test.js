bbb9ed1ee8f607de5313bbf36019b748727d588a
Afowowe Olumide Victor <volumide42@gmail.com>
2023-03-29 11:13:45 +0100

    text correction

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,7 +4,7 @@ const TestBullyBlox = () => {
   return (
     <Row>
       <h3 className="mb-4">Test BullyBloxx</h3>
-      <p>For a guided walk through of the BullyBloxx system please click on the audio below." add this to the page before dem come say rubbish again</p>
+      <p>For a guided walk through of the BullyBloxx system please click on the audio below.</p>
       <audio controls>
         <source src={audio} type="audio/wav" />
       </audio>
