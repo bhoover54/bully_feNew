d6d116587d9f1ca1f6fecc4c07263955edc411ce
Afowowe Olumide Victor <volumide42@gmail.com>
2023-04-08 12:31:46 +0100

    revert url

--- src/misc/url.js
+++ src/misc/url.js
@@ -1,4 +1,4 @@
-// const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5001/" : "https://bullybloxx.onrender.com/"
-const BASE_URL = process.env.NODE_ENV === "production" ? "http://localhost:5001/" : "https://bullyboxx-be.onrender.com/"
+const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5001/" : "https://bullybloxx.onrender.com/"
+// const BASE_URL = process.env.NODE_ENV === "production" ? "http://localhost:5001/" : "https://bullyboxx-be.onrender.com/"
 
 export default BASE_URL
