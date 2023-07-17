89115ae8f9547c416c346647745f6ea0c4a3c96d
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-17 12:16:57 -0400

    Update media.js

--- src/pages/media.js
+++ src/pages/media.js
@@ -13,7 +13,7 @@ const Media = () => {
 
   return (
     <Row>
-      <p className="text-center">P lease choose from the videos below to obtain the information that you are seeking.</p>
+      <p className="text-center">Please choose from the videos below to obtain the information that you are seeking.</p>
       {video.map((e) => (
         <Col xs="12" md="6">
           <iframe width="100%" height="400" src={`https://www.youtube.com/embed/${e.path}`} title={e.name} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
