0025bb7215654b4859ae918b76cbab4e91988036
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-19 01:48:20 -0400

    Update bully-free-school.js

--- src/pages/bully-free-school.js
+++ src/pages/bully-free-school.js
@@ -10,8 +10,8 @@ const BullyFreeSchool = () => {
       <p className="">
         Neighborhoods and communities want bully free schools for 4 main reasons:
         <ol>
-          <li> To provide a much safer environment for children in their schools.</li>
-          <li>To create an environment where children can thrive, socially and academically, learning to their fullest potential, without being afraid of anyone.</li>
+          <li> Bully-free schools are much safer environments for students.</li>
+          <li>Bully-free schools create an environment where children can thrive, socially and academically, learning to their fullest potential, without being afraid of anyone.</li>
           <li>To reap the financial rewards of living in a neighborhood with bully-free schools and the significant increase in property values gained from that. Bully-Free neighborhoods should also see a reduction in crime, especially domestic crime.</li>
           <li>Any responsible parent in today’s world would immediately seize an opportunity to have their children in a Bully-Free school</li>
         </ol>
