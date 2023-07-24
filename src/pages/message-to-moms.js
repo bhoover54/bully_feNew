c10169dafcba3f03dab639bb30f4b1f2e6283b64
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-24 12:26:25 -0400

    Update message-to-moms.js

--- src/pages/message-to-moms.js
+++ src/pages/message-to-moms.js
@@ -5,7 +5,7 @@ const MessageToMoms = () => {
     <Row>
       <Col xs="12" md="8" className="mx-auto">
         <h3 className="mb-3"> Message to Moms</h3>
-        <iframe width="100%" height="500" src="https://www.youtube.com/embed/J_bWh5pI7lA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        {/*<iframe width="100%" height="500" src="https://www.youtube.com/embed/J_bWh5pI7lA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
       </Col>
     </Row>
   )
