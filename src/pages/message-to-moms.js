649b64c9188f00d80e9a0956d7c5ccd53513c10b
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-24 20:45:29 -0400

    Update message-to-moms.js

--- src/pages/message-to-moms.js
+++ src/pages/message-to-moms.js
@@ -8,6 +8,14 @@ const MessageToMoms = () => {
         {/*<iframe width="100%" height="500" src="https://www.youtube.com/embed/J_bWh5pI7lA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
       </Col>
     </Row>
+        <p className="">
+  The real truth is this; parents, especially moms now have control over whether or not their child gets bullied at school. Single moms, the parents who have struggled the most from bullying, now have the same power as anyone else to protect their children from bullying.
+With this system parents can collectively establish a bully-free school environment for their children by doing 2 simple things:
+1- Sit down with your children regularly and tell them to please come to you anytime they see or experience bullying so it can be reported through this system. 
+2- When a bullying incident does occur follow the instructions in the TORPEDO YOUR BULLY tab to quickly shut the bully down.
+These 2 things are all that is asked of parents to end this war on bullying and provide their child with a school without bullying to go to.
+If you are a church youth leader, coach, scoutmaster or anyone else who leads young people please make sure every one of your members understands how important it is to use this system and end the bullying in their schools.
+  </p>
   )
 }
 
