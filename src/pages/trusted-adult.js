c39e2d9fb7236c95578f19553d518dd3df1f42e1
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-27 13:43:01 -0400

    Update trusted-adult.js

--- src/pages/trusted-adult.js
+++ src/pages/trusted-adult.js
@@ -5,7 +5,25 @@ const TrustedAdult = () => {
     <Row>
       <Col xs="12" md="8" className="mx-auto">
         <h3 className="mb-3">Trusted Adult</h3>
-        
+        <p className="">
+            Moms and trusted adults are the heroes behind The Bully Periscope System. Moms are the ones who encourage their children to use The Bully Periscope and report every bullying incident that they experience or witness……...trusted adults allow students to report these incidents without anyone knowing who they are………..not even the principal……...once the report has been filed the trusted adult becomes a “bully liaison” and passes questions and answers back and forth between the student and principal if any additional information on the bully is needed……..Trusted adults can live many states away and they can be family, friends, church youth leaders, scoutmasters, or anyone else who will help the child being victimized to stop this abuse……….
+            Here is the best way to help a student as their trusted adult:             
+        </p>
+        <p className="">
+            1- Choose a username and password and sign up. Place the letters TA at the end of the username.
+        </p>
+        <p className="">
+            2- Download a verification video (this is all explained on the bully report form page)    
+        </p>
+        <p className="">
+            3- Give your reporting student your sign in information and allow them to actually file the report through your account. This way the student can fill in all the information on the report form.
+        </p>
+        <p className="">
+            4- Once the student has filed the report have them copy the report and send it to you. This way you have the report in front of you if the principal needs more information.   
+        </p>
+        <p className="">
+            It’s that simple to help a student report abuse, and remember, you never tell ANYONE who the student is.
+        </p>
       </Col>
     </Row>
   )
