e554e306ac906fd329881808ffa14f823e3ae3aa
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-27 09:08:53 -0400

    Update policy.js

--- src/pages/policy.js
+++ src/pages/policy.js
@@ -3,14 +3,14 @@ import { Row } from "reactstrap"
 const Policy = () => {
   return (
     <Row>
-      <h3>Bully Discipline Policy</h3>
-      <p className="">If your school has in place a bully discipline policy that is not working contact your school board members and suggest that they begin using the BullyBloxx Bully Discipline Policy; it is very firm, extremely effective and completely fair to all parties involved.</p>
+      <h3>Bully Periscope Bully Discipline Policy</h3>
+      <p className="">If your school has in place a bully discipline policy that is not working contact your school board members and suggest that they begin using the Bully Periscope Bully Discipline Policy; it is very firm, extremely effective and completely fair to all parties involved.</p>
       <p>
         {" "}
-        BullyBloxx Discipline Policy
+        Bully Periscope Discipline Policy
         <ul>
           <li>
-            1st offense / Level 1 Bullying - The bully is only given a warning, shown the entire bullying discipline policy and presented the BullyBloxx Rule of Conduct; “keep your hands and your words to yourself. <br />
+            1st offense / Level 1 Bullying - The bully is only given a warning, shown the entire bullying discipline policy and presented the Bully Periscope Rule of Conduct; “keep your hands and your words to yourself. <br />
             This provides the student with the opportunity to correct their behavior and avoid the more severe consequences for bullying. The bully’s parents/guardians will immediately be notified of the bullying behavior committed by their student. School administrators can require counseling for
             both the student and their parents if necessary.
           </li>
@@ -19,7 +19,7 @@ const Policy = () => {
             second bullying offense. School administrators can require counseling for both the student and their parents if necessary. If the student is involved in athletics, cheerleading or any other type of extra- curricular activities, their coaches/sponsors will be immediately notified that the
             student has 2 offenses of bullying and one more offense will remove them from all sports/extra- curricular activities. <br />
             These coaches/sponsors will be expected to immediately become involved and provide all assistance in shutting down the student’s bullying behavior. These coaches/sponsors can levy their own consequences on the student for jeopardizing their responsibility to their team/group because of
-            bullying. The student is again presented the BullyBloxx Rule of Conduct; “keep your hands and your words to yourself.”
+            bullying. The student is again presented the  Bully Periscope Rule of Conduct; “keep your hands and your words to yourself.”
           </li>
           <li>
             3rd offense / Level 1 Bullying - The bully is no longer allowed to participate in any sport, cheer leading, or any type of extra-curricular activity for the remainder of the school year, nor can they attend any extra-curricular activity for the remainder of the school year. Their
@@ -27,7 +27,7 @@ const Policy = () => {
             They will receive 3 days of out of school suspension and will not be allowed to return to school until he/she and their parents/guardians take a tour of the alternative school that students attend when they are expelled from school. This clearly exhibits for bullies and their parents
             what the next step in this process delivers and allows the bully a final opportunity to correct their behavior. School administrators can require counseling for both the student and their parents if necessary.
             <br />
-            The student is again presented the BullyBloxx Rule of Conduct; “keep your hands and your words to yourself.
+            The student is again presented the Bully Periscope Rule of Conduct; “keep your hands and your words to yourself.
           </li>
           <li>4th offense / Level 1 Bullying – Recommendation to the school board for expulsion for the remainder of the school year.</li>
         </ul>
@@ -39,8 +39,8 @@ const Policy = () => {
         <li>The 2nd offense brings into the equation every coach/staff member that has direct contact with the bullying student and allows them to interject their influence/consequences on the bully. This is extremely valuable.</li>
         <li>The 3rd offense takes away all things from the bully (sports, groups, etc.) that are more satisfying to the bully than abusing their class mates. It also clearly communicates to the bully that this is the final wake up call; one more offense and school is literally out.</li>
         <li>
-          f a bully commits a 4th offense after all the efforts made to correct their behavior, they are proving that they are unwilling to carry themselves at the level of conduct that is required to attend “regular school.” Alternative schools are specifically for these types of students. The
-          people behind BullyBloxx believe this: Our first obligation is to well behaved students who respect their teachers, are good classmates and great friends who are attending school to learn.
+          If a bully commits a 4th offense after all the efforts made to correct their behavior, they are proving that they are unwilling to carry themselves at the level of conduct that is required to attend “regular school.” Alternative schools are specifically for these types of students. The
+          parents behind The Bully Periscope believe this: Our first obligation is to well behaved students who respect their teachers, are good classmates and great friends who are attending school to learn.
         </li>
       </ul>
     </Row>
