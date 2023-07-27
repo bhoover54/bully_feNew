0129308ba1f806d66f6e10493cdacf73f271158c
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-27 14:20:30 -0400

    Update bully-free-school.js

--- src/pages/bully-free-school.js
+++ src/pages/bully-free-school.js
@@ -1,23 +1,27 @@
-import { Row } from "reactstrap"
+import { Col, Row } from "reactstrap"
 
 const BullyFreeSchool = () => {
   return (
     <Row>
-      <h3>Bully Free Schools</h3>
-  <p className="">
-  In bully-free schools, students don't abuse each other. If an incident does occur, the abuser is immediately shut down before it happens again. When students and parents come together and use The Bully Periscope, this is the new school environment that they can create by driving bullying out of the school 
-      </p>
-      <p className="">
-        Neighborhoods and communities want bully free schools for 4 main reasons:
-        <ol>
-          <li> Bully-free schools are much safer environments for students.</li>
-          <li>Bully-free schools create an environment where children can thrive, socially and academically, learning to their fullest potential, without being afraid of anyone.</li>
-          <li>To reap the financial rewards of living in a neighborhood with bully-free schools and the significant increase in property values gained from that. Bully-Free neighborhoods should also see a reduction in crime, especially domestic crime.</li>
-          <li>Any responsible parent in today’s world would immediately seize an opportunity to have their children in a Bully-Free school</li>
-        </ol>
-      </p>
+        <Col xs="12" md="8" className="mx-auto">
+          <h3>Bully Free Schools</h3>
+          <p className="">
+          In bully-free schools, students don't abuse each other. If an incident does occur, the abuser is immediately shut down before it happens again. When students and parents come together and use The Bully Periscope, this is the new school environment that they can create by driving bullying out of the school 
+              </p>
+              <p className="">
+                Neighborhoods and communities want bully free schools for 4 main reasons:
+                <ol>
+                  <li> Bully-free schools are much safer environments for students.</li>
+                  <li>Bully-free schools create an environment where children can thrive, socially and academically, learning to their fullest potential, without being afraid of anyone.</li>
+                  <li>To reap the financial rewards of living in a neighborhood with bully-free schools and the significant increase in property values gained from that. Bully-Free neighborhoods should also see a reduction in crime, especially domestic crime.</li>
+                  <li>Any responsible parent in today’s world would immediately seize an opportunity to have their children in a Bully-Free school</li>
+                </ol>
+              </p>
 
-      <p className="">Please, look at the Message to Moms video at the top of The Bully Periscope homepage with your children. Follow the instructions there and bullying in the school will cease very quickly.</p>
+              <p className="">
+                Please, look at the Message to Moms video at the top of The Bully Periscope homepage with your children. Follow the instructions there and bullying in the school will cease very quickly.
+              </p>
+          </Col>
     </Row>
   )
 }
