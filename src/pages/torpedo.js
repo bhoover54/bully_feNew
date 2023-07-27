943d43be3b5e62d5db43cbc88e7e671a4ea975e4
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-27 19:22:59 -0400

    Update torpedo.js

--- src/pages/torpedo.js
+++ src/pages/torpedo.js
@@ -5,7 +5,25 @@ const Torpedo = () => {
     <Row>
       <Col xs="12" md="8" className="mx-auto">
         <h3 className="mb-3">Torpedo Your Bully/h3>
-        
+        <p className="">
+            The following procedures empowers parents, single or otherwise, to quickly end any abuse their child is enduring from bullying at school, and to stop this harm without needing assistance from anyone. We call this action Torpedoing Your Bully.            
+        </p>
+        <p className="">
+            The First Time the Bully Strikes:
+        </p>
+        <p className="">
+            The Bully Periscope has it’s own bully, threat and weapons reporting system. When any bully strikes, go to the Bully Reporting Form tab and report the incident; in fact, do this every time a bully strikes. In most cases reports are submitted for students by trusted adults. This provides 100% protection for the reporting student’s identity; not even the principal knows who they are. Please click on the TRUSTED ADULTS tab to learn all details involved in being a trusted adult.
+            Every bully report is downloaded in both the bully’s database as well as the school’s database. A copy of the bully report is sent to the principal with instructions to immediately shut the abuse from this bully down.
+        </p>
+        <p className="">
+            
+        </p>
+        <p className="">
+               
+        </p>
+        <p className="">
+            
+        </p>
       </Col>
     </Row>
   )
