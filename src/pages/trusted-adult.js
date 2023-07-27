0db5443d9601e662ec0283b76d852000d0bba54a
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-27 13:30:20 -0400

    Update trusted-adult.js

--- src/pages/trusted-adult.js
+++ src/pages/trusted-adult.js
@@ -24,12 +24,10 @@ const TrustedAdult = () => {
         <p className="">
             It’s that simple to help a student report abuse, and remember, you never tell ANYONE who the student is.
         </p>
-  
-        {/*<iframe width="100%" height="500" src="https://www.youtube.com/embed/J_bWh5pI7lA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
       </Col>
     </Row>
-        
   )
 }
 
 export default TrustedAdult
+
