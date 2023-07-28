7ecd8897769d854eb03ffdf8e6e328e5043ffd86
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-28 12:22:38 -0400

    Update letter1.js

--- src/pages/letter1.js
+++ src/pages/letter1.js
@@ -3,41 +3,24 @@ import { Row } from "reactstrap"
 const Letter1 = () => {
   return (
     <Row>
-      <p className="py-4">
-        <strong> Principal’s letter</strong> – <br /> If you are reporting the same bully for a 2nd time, email the letter below to the principal.
-      </p>
-      <p>
-        ________________ Date <br />
-        Dear ________________(Principal), <br />
-        I am sending you this letter as a follow up to the two reports I have filed against “Billy Bully” (the bully). Please accept this letter as notification if “Billy Bully” strikes again and a 3rd report is necessary I will take the following actions:
-        <br />
-        I will utilize The Periscope feature in the BullyBloxx database and obtain the information from every bully report that has been filed by anyone against “Billy Bully”. I will then send a copy of this letter with The Periscope Reports attached, as well as all communications between you and I
-        to all school board members and the Superintendent of the district.
-        <br />
-        Please understand that this letter is my attempt to get this abuse stopped before stronger measures are demanded. After contacting the superintendent and school board members, if this abuse is allowed to continue, I will have no other choice but to contact a personal injury attorney, turn
-        over all Periscope Reports and communications between us to them and let them take over this issue and end the abuse from this bully.
-        <br />
-        In closing I ask you to please bring “Billy Bully” in and take whatever actions are necessary to end their abuse so none of these actions listed in this letter will be necessary. If I can be of assistance in any way in this matter please feel free to contact me.
-        <br /> Sincerely,
+      <p className="">
+          If this is your situation please complete the letter below, (LETTER #1) copy and paste it in an email it to the principal with the heading Immediate Attention Requested 
+          along with a copy of your bully’s Periscope Report.
       </p>
       <p className="py-4">
-        <strong> Superintendent and Board Member’s letter </strong> – <br /> If you are reporting a bully for the 3rd time, email the letter below to the Superintendent and Board Members.
-      </p>
-      <p>
-        Letter to Superintendent and Board Members
-        <br />
-        ________________ Date <br />
-        Dear Superintendent and Board Members, <br />
-        I am sending you this letter as a follow up to the three reports I have filed against “Billy Bully” (the bully) with __________________ (Principal). Please find enclosed a letter I sent to ________________(Principal) after I filed the 2nd report against “Billy Bully” as well as a copy of
-        Billy Bully’s Periscope Report showing his bullying history.
-        <br />
-        Please accept this letter as notification if “Billy Bully” strikes again and a 4th report is necessary I will, at that point, be out of good options and I will have no choice but to contact a personal injury attorney, turn over all Periscope Reports and communications between us to them and
-        let them take over this issue and end the abuse from this bully.
-        <br />
-        In closing I ask you to please bring “Billy Bully” in and take whatever actions are necessary to end their abuse so no further actions will be necessary on this end. If I can be of assistance in any way in this matter please feel free to contact me.
-        <br />
-        Sincerely,
-      </p>
+        ________________ Date <br /><br />
+        Dear ________________(Principal), <br /><br />
+        I am sending you this letter as a follow up to the two reports I have filed against ___________________________(the bully). I have used The Bully Periscope feature to obtain the information from these bully reports. 
+        Please accept this letter as notification if ____________________ (the bully) strikes again and a 3rd report is necessary I will take the following actions:
+        <br /><br />
+        I will then send a copy of this letter with The Periscope Reports attached, as well as all communications between you and I to all school board members and the Superintendent of the district.
+        <br /><br />
+        Please understand that this letter is my attempt to get this abuse stopped before stronger measures are demanded. After contacting the superintendent and school board members, if this abuse is allowed to continue, 
+        I will have no other choice but to utilize social media and local news media to display copies of the Bully Periscope Report on this bully along with all communications between myself and school administrators to inform our community members of the level of neglect present in the school when addressing bullies and their abuse. I may also contact a personal injury attorney, turn over all Periscope Reports and communications between us over to them, and let them take over this issue and end the abuse from this bully.
+        <br /><br />
+        In closing I ask you to please bring ________________(the bully) in and take whatever actions are necessary to end their abuse so none of these actions listed in this letter will be necessary. If I can be of assistance in any way in this matter please feel free to contact me.
+        <br /><br /> Sincerely,
+      </p>      
     </Row>
   )
 }
