24c38b8b96d56333ce05d825e66d83849657f380
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-28 10:35:43 -0400

    Update torpedo.js

--- src/pages/torpedo.js
+++ src/pages/torpedo.js
@@ -1,6 +1,6 @@
 import { Col, Row } from "reactstrap"
 
-const TrustedAdt = () => {
+const Torpedo = () => {
   return (
     <Row>
       <Col xs="12" md="8" className="mx-auto">
@@ -29,5 +29,5 @@ const TrustedAdt = () => {
   )
 }
 
-export default TrustedAdt
+export default Torpedo
 
