0dfb72003a6c4a6f8f059c15cd76f2a73d02dcae
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-28 10:25:21 -0400

    Update torpedo.js

--- src/pages/torpedo.js
+++ src/pages/torpedo.js
@@ -1,33 +1,33 @@
 import { Col, Row } from "reactstrap"
 
-const Torpedo_Main = () => {
+const TrustedAdult = () => {
   return (
     <Row>
       <Col xs="12" md="8" className="mx-auto">
-        <h3 className="mb-3">Torpedo Your Bully/h3>
+        <h3 className="mb-3">Trusted Adult</h3>
         <p className="">
-            The following procedures empowers parents, single or otherwise, to quickly end any abuse their child is enduring from bullying at school, and to stop this harm without needing assistance from anyone. We call this action Torpedoing Your Bully.            
+            Moms and trusted adults are the heroes behind The Bully Periscope System. Moms are the ones who encourage their children to use The Bully Periscope and report every bullying incident that they experience or witness……...trusted adults allow students to report these incidents without anyone knowing who they are………..not even the principal……...once the report has been filed the trusted adult becomes a “bully liaison” and passes questions and answers back and forth between the student and principal if any additional information on the bully is needed……..Trusted adults can live many states away and they can be family, friends, church youth leaders, scoutmasters, or anyone else who will help the child being victimized to stop this abuse……….
+            Here is the best way to help a student as their trusted adult:             
         </p>
         <p className="">
-            The First Time the Bully Strikes:
+            1- Choose a username and password and sign up. Place the letters TA at the end of the username.
         </p>
         <p className="">
-            The Bully Periscope has it’s own bully, threat and weapons reporting system. When any bully strikes, go to the Bully Reporting Form tab and report the incident; in fact, do this every time a bully strikes. In most cases reports are submitted for students by trusted adults. This provides 100% protection for the reporting student’s identity; not even the principal knows who they are. Please click on the TRUSTED ADULTS tab to learn all details involved in being a trusted adult.
-            Every bully report is downloaded in both the bully’s database as well as the school’s database. A copy of the bully report is sent to the principal with instructions to immediately shut the abuse from this bully down.
+            2- Download a verification video (this is all explained on the bully report form page)    
         </p>
         <p className="">
-            
+            3- Give your reporting student your sign in information and allow them to actually file the report through your account. This way the student can fill in all the information on the report form.
         </p>
         <p className="">
-               
+            4- Once the student has filed the report have them copy the report and send it to you. This way you have the report in front of you if the principal needs more information.   
         </p>
         <p className="">
-            
+            It’s that simple to help a student report abuse, and remember, you never tell ANYONE who the student is.
         </p>
       </Col>
     </Row>
   )
 }
 
-export default Torpedo_Main
+export default TrustedAdult
 
