2bd55f3b8a946aad75765b0ee16ab23891db034e
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-28 15:20:43 -0400

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -96,7 +96,7 @@ const Home = () => {
       </Col>  
 
       <Col xs="12" md="8" className="mx-auto">
-        <h3 className="mb-3">Bully Periscope Report </h3>
+        <h3 className="mb-3">Bully Periscope Report Details </h3>
         <iframe width="100%" height="500" src="https://www.youtube.com/watch?v=rB7W1Wf2J7c" title="Bully Periscope Report Details" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </Col>
 
