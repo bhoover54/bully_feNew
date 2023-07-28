aa7942aeea0d82299af3db21ce351cfecdd99330
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-28 10:31:46 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Nav from "./components/nav"
 import Upload from "./pages/video"
 import Periscope from "./pages/periscope"
 import TrustedAdult from "./pages/trusted-adult"
-import TrustedAdult from "./pages/torpedo"
+import TrustedAdt from "./pages/torpedo"
 import AdminReport from "./pages/admin-report"
 import AdminSponsor from "./pages/admin-sponsors"
 import { ToastContainer } from "react-toastify"
@@ -54,7 +54,7 @@ function App() {
           <Route path="/school/admin" element={<SchoolAdmin />} />
           <Route path="/message-to-moms" element={<MessageToMoms />} />
           <Route path="/trusted-adult" element={<TrustedAdult />} />
-          <Route path="/torpedo" element={<TrustedAdult />} />
+          <Route path="/torpedo" element={<TrustedAdt />} />
           <Route path="/how-bullybloxx-works" element={<HowItWOrks />} />
           <Route path="/letters" element={<Letters />} />
           <Route path="/signin" element={<Login />} />
