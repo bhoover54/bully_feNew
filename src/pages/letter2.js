62dce4858195bce6fa8f2eaae8b5da6e4d638038
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-28 12:34:35 -0400

    Update letter2.js

--- src/pages/letter2.js
+++ src/pages/letter2.js
@@ -1,1 +1,28 @@
+import { Row } from "reactstrap"
 
+const Letter2 = () => {
+  return (
+    <Row>
+      <p className=""><b><i>
+          If this is your situation please complete the letter below, (LETTER #1) copy and paste it in an email it to the principal with the heading Immediate Attention Requested 
+          along with a copy of your bully’s Periscope Report.</i></b>
+      </p>
+      <p className="py-4">
+        ________________ Date <br /><br />
+        Dear ________________(Principal), <br /><br />
+        I am sending you this letter as a follow up to the two reports I have filed against ___________________________(the bully). I have used The Bully Periscope feature to obtain the information from these bully reports. 
+        Please accept this letter as notification if ____________________ (the bully) strikes again and a 3rd report is necessary I will take the following actions:
+        <br /><br />
+        I will then send a copy of this letter with The Periscope Reports attached, as well as all communications between you and I to all school board members and the Superintendent of the district.
+        <br /><br />
+        Please understand that this letter is my attempt to get this abuse stopped before stronger measures are demanded. After contacting the superintendent and school board members, if this abuse is allowed to continue, 
+        I will have no other choice but to utilize social media and local news media to display copies of the Bully Periscope Report on this bully along with all communications between myself and school administrators to inform our community members of the level of neglect present in the school when addressing bullies and their abuse. I may also contact a personal injury attorney, turn over all Periscope Reports and communications between us over to them, and let them take over this issue and end the abuse from this bully.
+        <br /><br />
+        In closing I ask you to please bring ________________(the bully) in and take whatever actions are necessary to end their abuse so none of these actions listed in this letter will be necessary. If I can be of assistance in any way in this matter please feel free to contact me.
+        <br /><br /> Sincerely,
+      </p>      
+    </Row>
+  )
+}
+
+export default Letter2
