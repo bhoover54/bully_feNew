1b6afe4d85485b208b7b19835b9a213e1460c220
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-28 13:59:07 -0400

    Update sit-3_2.js

--- src/pages/sit-3_2.js
+++ src/pages/sit-3_2.js
@@ -1,1 +1,25 @@
+import { Col, Row } from "reactstrap"
+
+const Sit_32 = () => {
+  return (
+    <Row>
+      <Col xs="12" md="8" className="mx-auto">
+        <h3 className="mb-3"> Message to Moms</h3>
+        <p className="">
+            <b><i>If you previously emailed letter #3 to the Superintendent and school board members please follow the instructions below:</i></b>
+        </p>
+        <p className="">
+            1 - Post all communications and letters along with the Bully Periscope Report for this bully on all social media so other parents and citizens are made aware of the 
+              dangerous levels of bullying that administrators are allowing in the schools and the level of neglect involving the handling of this particular bully.
+        </p>
+        <p className="">
+            2- Provide all communications and letters along with the Bully Periscope Report for this bully to all local media outlets so the entire community is made aware of the 
+              dangerous levels of bullying that administrators are allowing in the schools and the level of neglect involving the handling of this particular bully.
+        </p>
+      </Col>
+    </Row>
+  )
+}
+
+export default Sit_32
 
