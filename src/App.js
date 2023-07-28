5c4a8c84970f4da59f99af77d4d5c8ec646855d6
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-28 14:03:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ import Letters from "./pages/letters"
 import Letter1 from "./pages/letter1"
 import Letter2 from "./pages/letter2"
 import Letter3 from "./pages/letter3"
+import Letter4 from "./pages/letter4"
+import Sit_32 from "./pages/sit-3_2"
 import ForgotPassword from "./pages/forgot-password"
 import ResetPassword from "./pages/reset-paswod"
 import RealEstate from "./pages/real-estate"
@@ -63,6 +65,8 @@ function App() {
           <Route path="/letter1" element={<Letter1 />} />
           <Route path="/letter2" element={<Letter2 />} />
           <Route path="/letter3" element={<Letter3 />} />
+          <Route path="/letter4" element={<Letter4 />} />
+          <Route path="/sit-3_2" element={<Sit_32 />} />
           <Route path="/signin" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
