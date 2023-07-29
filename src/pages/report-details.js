6cd4cb89434bfb0239e8b05a504a07a6d62f465c
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-29 08:32:14 -0400

    Update report-details.js

--- src/pages/report-details.js
+++ src/pages/report-details.js
@@ -1,7 +1,16 @@
+import { Col, Row } from "reactstrap"
 
-<Col xs="12" md="8" className="mx-auto">
+const RptDtls = () => {
+  return (
+    <Row>
+        <Col xs="12" md="8" className="mx-auto">
         <h3 className="mb-3">Bully Periscope Report Details </h3>
         <iframe width="100%" height="400" src="https://youtube.com/embed/rB7W1Wf2J7c" title="Bully Periscope Report Details" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 
         {/*<iframe width="100%" height="500" src="https://www.youtube.com/watch?v=rB7W1Wf2J7c" title="Bully Periscope Report Details" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>*/}
       </Col>
+    </Row>
+  )
+}
+
+export default RptDtls
