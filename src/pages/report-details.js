7e24b79b004e928e60a54e8a60f8fd43c67814ea
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-29 08:29:05 -0400

    Update report-details.js

--- src/pages/report-details.js
+++ src/pages/report-details.js
@@ -1,1 +1,7 @@
 
+<Col xs="12" md="8" className="mx-auto">
+        <h3 className="mb-3">Bully Periscope Report Details </h3>
+        <iframe width="100%" height="400" src="https://youtube.com/embed/rB7W1Wf2J7c" title="Bully Periscope Report Details" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+        {/*<iframe width="100%" height="500" src="https://www.youtube.com/watch?v=rB7W1Wf2J7c" title="Bully Periscope Report Details" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>*/}
+      </Col>
