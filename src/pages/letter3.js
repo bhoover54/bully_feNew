5bab26909254ffb06fb18f868d8a622216965189
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-07-29 08:20:31 -0400

    Update letter3.js

--- src/pages/letter3.js
+++ src/pages/letter3.js
@@ -13,9 +13,9 @@ const Letter3 = () => {
         I am sending you this letter as a follow up to the two bully reports that I have filed against ____________________(the bully) with __________________ (Principal). Please find enclosed a copy of ___________________’s (the bully) Periscope Report showing that in addition to my 2 reports 
         this bully has been reported for 2 or more other incidents of bullying, making a total number of reports against this bully 4 or more. 
         <br /><br />
-        <b>This bully has now exceeded the acceptable number of bully abuses committed against others in a calendar school year (3) and they must be addressed 
+        This bully has now exceeded the acceptable number of bully abuses committed against others in a calendar school year (3) and they must be addressed 
         with their bullying immediately shut down. Failure to do so will leave me with no other choice but to utilize social media and local news media to display copies of the Bully Periscope Report on this bully along with all communications between myself and school administrators to 
-        inform our community members of the level of neglect present in the school when addressing bullies and their abuse. I may also contact a personal injury attorney, turn over all Periscope Reports and communications between us to them, and let them take over this issue and end the abuse from this bully.</b>
+        inform our community members of the level of neglect present in the school when addressing bullies and their abuse. I may also contact a personal injury attorney, turn over all Periscope Reports and communications between us to them, and let them take over this issue and end the abuse from this bully.
         <br /><br />
         In closing I ask you to please bring ____________________(the bully) in and take whatever actions are necessary to end their abuse so no further actions will be necessary on this end. If I can be of assistance in any way in this matter please feel free to contact me.
         <br /><br /> 
