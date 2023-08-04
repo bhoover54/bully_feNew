9758abd28ac475703672dd4188be6a684a999162
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-08-04 11:29:37 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <Row>
       <h3>About Us</h3>
-      <p>My name is Mack Galphin and I led in the development of BullyBloxx. Neighborhoods and communities want bully free schools for 4 main reasons:</p>
+      <p>My name is Mack Galphin and I led in the development of The Bully Periscope. Neighborhoods and communities want bully free schools for 4 main reasons:</p>
       <p>
         <ol>
           <li>To provide a much safer environment for children in their schools.</li>
@@ -15,10 +15,10 @@ const About = () => {
       </p>
       <p>
         The question we are asked most is what does “bully-free” schools mean; our answer is this. To most parents and citizens it means no student is allowed to abuse anyone in their school. If an incident does occur, the abuser is immediately addressed and shut down before it happens again. This
-        is the bully-free standard that most communities set and the standard that BullyBloxx provides.
+        is the bully-free standard that most communities set and the standard that The Bully Periscope provides.
       </p>
       <p>
-        Communities are demanding a system that quickly shuts down the abuse, school shootings and suicides because of bullying; and keeps them shut down. We developed BullyBloxx to fill this need for communities who are making their schools bully-free. This BullyBloxxs system is very fair to
+        Communities are demanding a system that quickly shuts down the abuse, school shootings and suicides because of bullying; and keeps them shut down. We developed The Bully Periscope to fill this need for communities who are making their schools bully-free. This The Bully Periscopes system is very fair to
         students, parents and administrators alike, but it is also very firm for all involved. With BulllyBloxx everybody is accountable for their behavior towards others, as it should be.
       </p>
       <p>
@@ -38,12 +38,12 @@ const About = () => {
 
       <p>
         By allowing the epidemic levels of bullying in our schools today we are teaching our children to abuse even those who they spend their lives with, those who are closest to them and that bleeds over into every part of their lives. Bullying is a 100% learned behavior, all these bullies in
-        schools were taught by someone, they weren’t born bullies. The good thing is a learned behavior like bullying can also be unlearned and that is one of the things that BullyBloxx does, it unlearns bullying behavior with one simple rule that we call The James Wilson Rule of Discipline, please
+        schools were taught by someone, they weren’t born bullies. The good thing is a learned behavior like bullying can also be unlearned and that is one of the things that The Bully Periscope does, it unlearns bullying behavior with one simple rule that we call The James Wilson Rule of Discipline, please
         allow me to explain:
       </p>
 
       <p>
-        A man named James Wilson was my first black teacher and a great influence on me, first as a student and later as a board member at his school. What I learned from him was invaluable in developing this BullyBloxx system. I met him when our schools integrated in 1970 in Ninety Six, S.C. Ninety
+        A man named James Wilson was my first black teacher and a great influence on me, first as a student and later as a board member at his school. What I learned from him was invaluable in developing this The Bully Periscope system. I met him when our schools integrated in 1970 in Ninety Six, S.C. Ninety
         Six was a small, uneducated, openly racist, cotton mill town in the South Carolina upstate that was about 60% white. When the schools were forced to integrate, I was with the first group of white students to attend Edgewood School which was previously the black school. With integration it
         was now the Middle School for about 300 students; I was in the 7th grade. Coach Wilson was the boys P.E. teacher and every boy in the school, about 150 of us, had class with him every day.
       </p>
@@ -55,26 +55,26 @@ const About = () => {
       </p>
 
       <p>
-        The James Wilson Rule of Discipline still works just as effectively today, there is no abuse when kids keep their hands and their mouths to themselves. Of course we are much softer today than we were in 1970 so the rule today is “keep your hands and words to yourself”. At BullyBloxx we
+        The James Wilson Rule of Discipline still works just as effectively today, there is no abuse when kids keep their hands and their mouths to themselves. Of course we are much softer today than we were in 1970 so the rule today is “keep your hands and words to yourself”. At The Bully Periscope we
         believe that it is time for all communities to tell all of their bullies in all of their schools exactly what Coach James Wilson told all of us boys on day #1 of integrating the schools way back in sept of 1970 in Ninety Six, S.C. He said ”you are going to keep your hands and your mouth to
         yourself”, and he meant it and our communities need to mean it too. We as communities need to make it very clear that those students who can’t keep their hands and their mouths to themselves can attend school at Alternative School until they learn how. These communities can utilize the
-        BullyBloxx system to establish this rule and by doing so make their schools bully-free.
+        The Bully Periscope system to establish this rule and by doing so make their schools bully-free.
       </p>
 
       {/* <p>
-        The citizens in these communities demand a system that quickly shuts down the abuse, school shootings and suicides because of bullying and keeps them shut down.........We developed BullyBloxx to fill this need for communities who were making their schools bully-free. .....By allowing the
+        The citizens in these communities demand a system that quickly shuts down the abuse, school shootings and suicides because of bullying and keeps them shut down.........We developed The Bully Periscope to fill this need for communities who were making their schools bully-free. .....By allowing the
         epidemic levels of bullying in our schools today we are teaching our children to abuse even those who they spend their lives with...........those who are closest to them. That bleeds over into every part of their lives..........bullying is a 100% learned behavior..........all these bullies
-        in schools were taught by someone, they weren’t born bullies...........the good thing is a learned behavior like bullying can also be unlearned...........and that is one of the things that bullybloxx does......... it unlearns bullying behavior with one simple rule.........here is how........
-        A man named James Wilson was my first black teacher and a great influence on me, first as a student and later as a board member at his school......... What I learned from him was invaluable in developing this BullyBloxx system. I met him when our schools integrated in 1970 in Ninety Six,
+        in schools were taught by someone, they weren’t born bullies...........the good thing is a learned behavior like bullying can also be unlearned...........and that is one of the things that The Bully Periscope does......... it unlearns bullying behavior with one simple rule.........here is how........
+        A man named James Wilson was my first black teacher and a great influence on me, first as a student and later as a board member at his school......... What I learned from him was invaluable in developing this The Bully Periscope system. I met him when our schools integrated in 1970 in Ninety Six,
         S.C. Ninety Six was a small, uneducated, openly racist, cotton mill town. Student ratio was about 60% white. When the schools were forced to integrate I was with the first group of white students to attend Edgewood School which was previously the black school and now the Middle School for
         about 300 students; I was in the 7 th grade. Coach Wilson was the boys P.E. teacher and every boy in the school had class with him every day. He was a former Marine boxing champ and about 35 years old. He was a superior athlete and nobody in the school could compete with him. He was the
         school’s disciplinarian for all the boys..........you got in trouble you went to him.......On day #1 he gave every boy in that school a rule to follow........it was real simple...........”keep your hands and your mouth to yourself”...........with that one rule he intended to integrate a
         middle school in a small, openly racist south carolina mill town and do it peacefully...........and he did just that.........the truth is, we had a ball.........both black and white..........there was always lots of fun and laughing out loud........we became friends......... This same rule
         will work today......let’s stop addressing the victims and let’s address the bullies for a change.........bullying is not the victim’s problem to solve.........in fact, victims have no way of solving the bullying problem.........the only one who can solve the bullying problem is the bully
-        and we at BullyBloxx believe that it is time for all communities to tell all of their bullies in all of their schools exactly what Coach James Wilson told all of us boys on day #1 of integrating the schools...... way back in sept of 1970......in Ninety Six, S.C...............He said ”you are
-        going to keep your hands and your mouth to yourself”........and when the community tells the bullies this, mean it......just like Coach Wilson did................. BullyBloxx gives the community the ability and the option to enforce Coach Wilson’s rule on every bully..............personally,
-        i think it’s the answer..........and I have seen it work...that is why I developed BullyBloxx....”keep your hands and your mouth to yourself”.........it’s really simple.........except today, 52 years later, we say..........”keep your hands and your words to yourself”....... Every community
-        makes it’s own decision on bully-free schools. Communities that desire bully-free schools have BullyBloxx to help achieve that. BullyBloxx provides the school administrators with a system that gets bullying reported and the support of the community to shut it down after it’s reported.
+        and we at The Bully Periscope believe that it is time for all communities to tell all of their bullies in all of their schools exactly what Coach James Wilson told all of us boys on day #1 of integrating the schools...... way back in sept of 1970......in Ninety Six, S.C...............He said ”you are
+        going to keep your hands and your mouth to yourself”........and when the community tells the bullies this, mean it......just like Coach Wilson did................. The Bully Periscope gives the community the ability and the option to enforce Coach Wilson’s rule on every bully..............personally,
+        i think it’s the answer..........and I have seen it work...that is why I developed The Bully Periscope....”keep your hands and your mouth to yourself”.........it’s really simple.........except today, 52 years later, we say..........”keep your hands and your words to yourself”....... Every community
+        makes it’s own decision on bully-free schools. Communities that desire bully-free schools have The Bully Periscope to help achieve that. The Bully Periscope provides the school administrators with a system that gets bullying reported and the support of the community to shut it down after it’s reported.
       </p> */}
     </Row>
   )
