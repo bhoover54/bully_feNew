348cd29112d6298a0f91d09d51c7a8860b879e10
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-08-04 11:34:29 -0400

    Update ambasador.js

--- src/pages/ambasador.js
+++ src/pages/ambasador.js
@@ -11,9 +11,9 @@ const Ambassador = () => {
         <br /> <br /> By stepping forward you are not only providing a very critical service to your community, you are also creating new relationships with the thousands of families connected to your school/schools. This is what makes Bully Shutdown Ambassador positions lead generating machines for
         agents.
       </p>
-      <p>Ambassadors receive very interesting and informative articles from BullyBloxx every 3-4 weeks by email. Ambasadors have only one responsibility; to post this information on all of their social media accounts for the parents of the school and members of the community to see.</p>
+      <p>Ambassadors receive very interesting and informative articles from The Bully Periscope every 3-4 weeks by email. Ambasadors have only one responsibility; to post this information on all of their social media accounts for the parents of the school and members of the community to see.</p>
       <p>
-        Below is a sample of the initial article posted by all Ambassadors, it introduces you and BullyBloxx to the community. As you see in this sample, the name of the sample school is Kex Middle School, the agent is Jordan Guinn and the agency is Emma Watkins Realty of Camak Texas. Of course you
+        Below is a sample of the initial article posted by all Ambassadors, it introduces you and The Bully Periscope to the community. As you see in this sample, the name of the sample school is Kex Middle School, the agent is Jordan Guinn and the agency is Emma Watkins Realty of Camak Texas. Of course you
         will want to substitute your information and your school’s information for this. The heading of your posting on your social media should be “ATTENTION: All Kex Middle School Parents” (again, substituting your school’s name for Kex Middle School)
         <br />
         There are 2 way that you can post this information on your social media:
@@ -27,16 +27,16 @@ const Ambassador = () => {
       </p>
       <p className="py-2">
         Hi, I am Jordan Guinn of Emma Watkins Realty in Camak, Texas. If you are a Kex Middle School parent or a member of the Kex Middle School community please allow me to also introduce myself as the Bully Shutdown Ambassador for Kex Middle School. Kex Middle School students are now protected by
-        a new, state of the art system called BullyBloxx that quickly shuts down bullying and by doing so virtually eliminates the possibility of a school shooting or bullycide. <br />
-        BullyBloxx is a highly effective, parent run, community funded school shooter and bullying shut down system that absolutely works. BullyBloxx is very inexpensive to provide for the students, costing only $75 per week for any school no matter how large, which is pennies compared to the
-        thousands of dollars per month that many of today’s failed anti-bullying systems charge. Please go to bullybloxx.com and review the WHAT IS BULLYBLOXX main video and then watch the Message to Moms video with your child.
+        a new, state of the art system called The Bully Periscope that quickly shuts down bullying and by doing so virtually eliminates the possibility of a school shooting or bullycide. <br />
+        The Bully Periscope is a highly effective, parent run, community funded school shooter and bullying shut down system that absolutely works. The Bully Periscope is very inexpensive to provide for the students, costing only $75 per week for any school no matter how large, which is pennies compared to the
+        thousands of dollars per month that many of today’s failed anti-bullying systems charge. Please go to thebullyperiscope.com and review the WHAT IS THE BULLY PERISCOPE main video and then watch the Message to Moms video with your child.
         <br />
-        This system is fully operational, right now as we speak, providing all of its protections for Kex Middle School students. BullyBloxx is providing this system free for 45 days to allow the community time to provide the funds to support it for the first year.
+        This system is fully operational, right now as we speak, providing all of its protections for Kex Middle School students. The Bully Periscope is providing this system free for 45 days to allow the community time to provide the funds to support it for the first year.
         <br />
-        Once you have reviewed this system and seen how quickly and effectively it shuts down bullying, ask yourself, would our Kex Middle School students be safer with or without BullyBloxx. If you agree that this is a “must have system” for our students at Kex Middle School, please click on the
-        DONATE FOR YOUR SCHOOL tab on the bullybloxx.com home page and donate to fund this very important protection for our Kex Middle School students.
+        Once you have reviewed this system and seen how quickly and effectively it shuts down bullying, ask yourself, would our Kex Middle School students be safer with or without The Bully Periscope. If you agree that this is a “must have system” for our students at Kex Middle School, please click on the
+        DONATE FOR YOUR SCHOOL tab on the thebullyperiscope.com home page and donate to fund this very important protection for our Kex Middle School students.
         <br />
-        If you have children in other schools, please go to bullybloxx.com and see how to quickly get them and their schools protected by this system. And please, share this on all of your social media so parents in all of our schools can be aware of this protection for their children.
+        If you have children in other schools, please go to thebullyperiscope.com and see how to quickly get them and their schools protected by this system. And please, share this on all of your social media so parents in all of our schools can be aware of this protection for their children.
         <br />
         Of course, if you have any real estate needs I am always here to serve you. Please text or call me at 992-543-3763 or email me at jguinn96@emmawatkinsrealty.com Thank you
         <br />
