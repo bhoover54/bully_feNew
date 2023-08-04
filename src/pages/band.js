63f86778be728e8dd21f261417650190b85c0217
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-08-04 11:37:19 -0400

    Update band.js

--- src/pages/band.js
+++ src/pages/band.js
@@ -8,9 +8,9 @@ const BandBooster = () => {
         Hello and congratulations if you have just become a Bully Shutdown Ambassador. Please know that we do not reveal the identity of any Band Booster Parent. By stepping forward you are providing a very critical service for your students and your community. You can serve as a Bully Shutdown
         Ambassador for up to 3 schools. Please work with your group to get all schools in the district covered with this protection, including elementary schools; the harmful bullying usually starts in the 4th grade.
       </p>
-      <p>Ambassadors receive very interesting and informative articles from BullyBloxx every 3-4 weeks by email. Ambassadors have only one responsibility; to post this information on all of their social media accounts for the parents of the school and members of the community to see.</p>
+      <p>Ambassadors receive very interesting and informative articles from The Bully Periscope every 3-4 weeks by email. Ambassadors have only one responsibility; to post this information on all of their social media accounts for the parents of the school and members of the community to see.</p>
       <p>
-        Below is a sample of the very first article posted by all Ambassadors that introduces BullyBloxx to the school community. As you see in this sample, the name of the sample school is Kex Middle School, of course you will want to substitute your information and your school’s information for
+        Below is a sample of the very first article posted by all Ambassadors that introduces The Bully Periscope to the school community. As you see in this sample, the name of the sample school is Kex Middle School, of course you will want to substitute your information and your school’s information for
         this. Here is the initial posting, please post this notice on all of your social media.
       </p>
       <p>
@@ -18,25 +18,25 @@ const BandBooster = () => {
         “ATTENTION: All Kex Middle School Parents and Students” (again, substituting your school’s name for Kex Middle School)
       </p>
       <p>
-        If you are a Kex Middle School parent or a member of the Kex Middle School community this message is to inform you that Kex Middle School students are now protected by a new, state of the art system called BullyBloxx that quickly shuts down bullying and by doing so virtually eliminates the
+        If you are a Kex Middle School parent or a member of the Kex Middle School community this message is to inform you that Kex Middle School students are now protected by a new, state of the art system called The Bully Periscope that quickly shuts down bullying and by doing so virtually eliminates the
         possibility of a school shooting or bullycide.
       </p>
 
       <p>
-        BullyBloxx is a highly effective, parent run, community funded school shooter and bullying shut down system that absolutely works. BullyBloxx is very inexpensive to provide for the students, costing only $75 per week for any school no matter how large, which is pennies compared to the
-        thousands of dollars per month that many of today’s failed anti-bullying systems charge. Please go to www.bullybloxx.com and review the WHAT IS BULLYBLOXX main video and then watch the Message to Moms video with your child.
+        The Bully Periscope is a highly effective, parent run, community funded school shooter and bullying shut down system that absolutely works. The Bully Periscope is very inexpensive to provide for the students, costing only $75 per week for any school no matter how large, which is pennies compared to the
+        thousands of dollars per month that many of today’s failed anti-bullying systems charge. Please go to www.thebullyperiscope.com and review the WHAT IS THE BULLY PERISCOPE main video and then watch the Message to Moms video with your child.
       </p>
 
-      <p>This system is fully operational, right now as we speak, providing all of its protections for Kex Middle School students. BullyBloxx is providing this system free for 45 days to allow the community time to provide the funds to support it for the first year.</p>
+      <p>This system is fully operational, right now as we speak, providing all of its protections for Kex Middle School students. The Bully Periscope is providing this system free for 45 days to allow the community time to provide the funds to support it for the first year.</p>
 
       <p>
-        If you would like a complete walk through of the system to see how every function works please click on the MORE tab and then click on the TEST BULLYBLOXX tab. Once you have reviewed this system and seen how quickly and effectively it shuts down bullying, ask yourself, would our Kex Middle
-        School students be safer with or without BullyBloxx. If you agree that this is a “must have system” for our students at Kex Middle School, please click on the DONATE FOR YOUR SCHOOL tab on the bullybloxx.com home page and donate to fund this very important protection for our Kex Middle
+        If you would like a complete walk through of the system to see how every function works please click on the MORE tab and then click on the TEST THE BULLY PERISCOPE tab. Once you have reviewed this system and seen how quickly and effectively it shuts down bullying, ask yourself, would our Kex Middle
+        School students be safer with or without The Bully Periscope. If you agree that this is a “must have system” for our students at Kex Middle School, please click on the DONATE FOR YOUR SCHOOL tab on the The Bully Periscope.com home page and donate to fund this very important protection for our Kex Middle
         School students.
       </p>
 
       <p>
-        If you have children in other schools, please go to bullybloxx.com and see how to quickly get them and their schools protected by this system. And please, share this on all of your social media so parents in all of our schools can be aware of this system and secure this protection for their
+        If you have children in other schools, please go to thebullyperiscope.com and see how to quickly get them and their schools protected by this system. And please, share this on all of your social media so parents in all of our schools can be aware of this system and secure this protection for their
         children.
       </p>
 
