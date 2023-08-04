03136d86fc10ce1cb9960fc9b7cfba11cd0ef53b
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-08-04 11:38:36 -0400

    Update footer.js

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
   <div className="mt-5 bg-danger text-white py-3">
     <div className="d-flex justify-content-center align-items-center gap-3  ">
-      <small className="text-center d-block">&copy; All rights reserved, BullyBloxx {new Date().getFullYear()}</small>
+      <small className="text-center d-block">&copy; All rights reserved, The Bully Periscope {new Date().getFullYear()}</small>
     </div>
   </div>
 )
