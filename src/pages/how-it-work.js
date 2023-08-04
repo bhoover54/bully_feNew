767ed71c412d215ec967226c42b53cec77b5b634
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-08-04 11:40:46 -0400

    Update how-it-work.js

--- src/pages/how-it-work.js
+++ src/pages/how-it-work.js
@@ -4,8 +4,8 @@ const HowItWOrks = () => {
   return (
     <Row>
       <Col xs="12" md="8" className="mx-auto">
-        <h3 className="mb-3">How BullyBloxx Works </h3>
-        <iframe width="100%" height="500" src="https://www.youtube.com/embed/fziG3WmNjkY" title="how bullyblox works" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <h3 className="mb-3">How The Bully Periscope Works </h3>
+        <iframe width="100%" height="500" src="https://www.youtube.com/embed/fziG3WmNjkY" title="how the bully periscope works" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </Col>
     </Row>
   )
