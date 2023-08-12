b3bd68317f24cbf8de04b20f264217e4a5554a17
bhoover54 <135153329+bhoover54@users.noreply.github.com>
2023-08-11 22:43:07 -0400

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -30,7 +30,7 @@ const Home = () => {
         </div>
       </Col>
       <Col md="6" className="d-flex flex-column justify-content-center p-3">
-        <h3 className=""> Once The Bully Periscope “Torpedoes” a Bully, it’s GAME OVER!<br />
+        <h3 className=""> When The Bully Periscope “Torpedoes” a Bully, School is Out......LITERALLY!<br />
                           
         </h3>
         <div className="">
